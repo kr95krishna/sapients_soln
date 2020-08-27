3058d27a0d6b8adedef7e321e03ab148aa4d5b01
kr95krishna <58079742+kr95krishna@users.noreply.github.com>
2020-08-27 10:04:47 +0530

    Update Mission.js

--- src/user/pages/Mission.js
+++ src/user/pages/Mission.js
@@ -4,7 +4,7 @@
   export default class Abc extends Component {
     state = {
       loading : true,
-      person : null
+      mission : null
     };
     async componentDidMount() {
       const url = "https://api.spacexdata.com/v3/launches?limit=100";
@@ -12,17 +12,17 @@
       const data = await response.json();
     //   console.log(data[0].flight_number);
        // console.log(data.results[0]);
-     this.setState({person: data, loading: false});
-     console.log(this.state.person);
+     this.setState({mission: data, loading: false});
+     console.log(this.state.mission);
     }
     render(){
       return(
         <div>
-         {this.state.loading || !this.state.person ? (
+         {this.state.loading || !this.state.mission ? (
             <div>loading..</div> 
           ) : ( 
             <div>
-             <div> <MissionList items = {this.state.person} /> </div>
+             <div> <MissionList items = {this.state.mission} /> </div>
          </div>
           )}
           </div>
