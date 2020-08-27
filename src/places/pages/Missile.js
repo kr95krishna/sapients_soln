e5d54c70d7ff48e40f07d87816fb2fd15dbd02af
kr95krishna <58079742+kr95krishna@users.noreply.github.com>
2020-08-27 10:07:28 +0530

    Update Missile.js

--- src/places/pages/Missile.js
+++ src/places/pages/Missile.js
@@ -4,7 +4,7 @@ import MissionList from '../../user/components/MissionList';
 export default class Abc extends Component {
   state = {
     loading : true,
-    person : null
+     mission : null
   };
   async componentDidMount() {
     const url = "https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true";
@@ -13,13 +13,13 @@ export default class Abc extends Component {
   //   console.log(data[0].flight_number);
      // console.log(data.results[0]);
      console.log(this.props.value);
-   this.setState({person: data});
-   console.log(this.state.person);
+   this.setState({mission: data});
+   console.log(this.state.mission);
   }
   render(){
     return(
       <div>
-       {this.state.person && <MissionList items = {this.state.person} />}
+       {this.state.mission && <MissionList items = {this.state.mission} />}
        </div>
     );
   }
